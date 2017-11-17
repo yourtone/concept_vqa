accdc2cf70c9bbaa99bf5410a5a87b79ca81a305
Zhangyang Pang <pzy@zju.edu.cn>
2017-11-17 22:19:05 +0800

    Avoid loading VQA data when import eval_tools

--- eval_tools.py
+++ eval_tools.py
@@ -10,27 +10,37 @@ import os
 import tempfile
 from operator import itemgetter
 
-que_fname_ptn = '{}/Questions/v2_OpenEnded_mscoco_{}_questions.json'
-ann_fname_ptn = '{}/Annotations/v2_mscoco_{}_annotations.json'
 
-trn_que_fname = que_fname_ptn.format(cfg.VQA_DIR, 'train2014')
-trn_ann_fname = ann_fname_ptn.format(cfg.VQA_DIR, 'train2014')
-trn_vqa = VQA(trn_ann_fname, trn_que_fname)
+class VQATool(object):
+    _instances = {}
 
-val_que_fname = que_fname_ptn.format(cfg.VQA_DIR, 'val2014')
-val_ann_fname = ann_fname_ptn.format(cfg.VQA_DIR, 'val2014')
-val_vqa = VQA(val_ann_fname, val_que_fname)
+    def __init__(self):
+        self.que_fptn = '{}/Questions/v2_OpenEnded_mscoco_{}_questions.json'
+        self.ann_fptn = '{}/Annotations/v2_mscoco_{}_annotations.json'
+
+
+    def get_que_path(self, vqa_dir, split):
+        return self.que_fptn.format(vqa_dir, split)
+
+
+    def get_ann_path(self, vqa_dir, split):
+        return self.ann_fptn.format(vqa_dir, split)
+
+
+    def get_vqa(self, vqa_dir, split):
+        if split not in self._instances:
+            que_fname = self.get_que_path(vqa_dir, split)
+            ann_fname = self.get_ann_path(vqa_dir, split)
+            self._instances[split] = VQA(ann_fname, que_fname)
+        return self._instances[split]
 
 
 def get_eval(result, split):
-    if split == 'train2014':
-        vqa = trn_vqa
-        que_fname = trn_que_fname
-    elif split == 'val2014':
-        vqa = val_vqa
-        que_fname = val_que_fname
-    else:
+    if split not in ('train2014', 'val2014'):
         raise ValueError('split must be "train2014" or "val2014"')
+    vqa_tool = VQATool()
+    vqa = vqa_tool.get_vqa(cfg.VQA_DIR, split)
+    que_fname = vqa_tool.get_que_path(cfg.VQA_DIR, split)
 
     res_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
     json.dump(result, res_file)
