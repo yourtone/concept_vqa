3684ff02a6383147ef5c9f1ae03d1fb18f29f315
Zhangyang Pang <pzy@zju.edu.cn>
2018-01-04 18:05:49 +0800

    Support selecting device in ensemble script

--- ensemble.py
+++ ensemble.py
@@ -18,6 +18,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('model_dir')
 parser.add_argument('--bs', '--batch_size', default=128, type=int,
                     help='batch size for predicting')
+parser.add_argument('--gpu_id', default=0, type=int, metavar='N',
+                    help='index of the gpu')
 parser.add_argument('--cfg', dest='cfg_file', default=None, type=str,
                     help='optional config file')
 parser.add_argument('--set', dest='set_cfgs', default=None,
@@ -25,12 +27,16 @@ parser.add_argument('--set', dest='set_cfgs', default=None,
 
 
 def main():
+    args = parser.parse_args()
     if args.cfg_file is not None:
         cfg_from_file(args.cfg_file)
     if args.set_cfgs is not None:
         cfg_from_list(args.set_cfgs)
 
-    args = parser.parse_args()
+    # select device
+    torch.cuda.set_device(args.gpu_id)
+    print('[Info] use gpu: {}'.format(torch.cuda.current_device()))
+
     files = os.listdir(args.model_dir)
     model_files = [f for f in files if f.endswith('.pth.tar')]
     model_info = []
@@ -49,6 +55,8 @@ def main():
 
     vote_buff = [{} for i in range(len(dataset))]
     conf_buff = np.zeros((len(dataset), len(itoa)))
+    sm_conf_buff = np.zeros((len(dataset), len(itoa)))
+    l2_conf_buff = np.zeros((len(dataset), len(itoa)))
     que_ids = dataset.que_id
     for model_group_name, model_name, model_acc, cp_file in model_info:
         # data
@@ -75,9 +83,14 @@ def main():
         for sample in bar(dataloader):
             sample_var = [Variable(d).cuda() for d in list(sample)[1:]]
             score = model(*sample_var)
+            sm_score = torch.nn.functional.softmax(score)
+            l2_score = torch.nn.functional.normalize(score)
 
             bs = score.size(0)
             conf_buff[start:start+bs] += score.data.cpu().numpy()
+            sm_conf_buff[start:start+bs] += sm_score.data.cpu().numpy()
+            l2_conf_buff[start:start+bs] += l2_score.data.cpu().numpy()
+
             _, ans_ids = torch.max(score.data, dim=1)
             for i, ans_id in enumerate(ans_ids):
                 ans = itoa[ans_id]
@@ -86,18 +99,35 @@ def main():
 
             start += bs
 
+    # origin score ensemble
     _, ans_ids = torch.max(torch.from_numpy(conf_buff), dim=1)
     conf_result = []
     for que_id, ans_id in zip(que_ids, ans_ids):
         conf_result.append({'question_id': int(que_id), 'answer': itoa[ans_id]})
+    fname = os.path.join(args.model_dir, 'result-conf.json')
+    json.dump(conf_result, open(fname, 'w'))
 
+    # softmax score ensemble
+    _, ans_ids = torch.max(torch.from_numpy(sm_conf_buff), dim=1)
+    conf_result = []
+    for que_id, ans_id in zip(que_ids, ans_ids):
+        conf_result.append({'question_id': int(que_id), 'answer': itoa[ans_id]})
+    fname = os.path.join(args.model_dir, 'result-sm-conf.json')
+    json.dump(conf_result, open(fname, 'w'))
+
+    # l2 normalized score ensemble
+    _, ans_ids = torch.max(torch.from_numpy(l2_conf_buff), dim=1)
+    conf_result = []
+    for que_id, ans_id in zip(que_ids, ans_ids):
+        conf_result.append({'question_id': int(que_id), 'answer': itoa[ans_id]})
+    fname = os.path.join(args.model_dir, 'result-l2-conf.json')
+    json.dump(conf_result, open(fname, 'w'))
+
+    # voting ensemble
     vote_buff = [Counter(v).most_common(1)[0][0] for v in vote_buff]
     vote_result = []
     for que_id, ans in zip(que_ids, vote_buff):
         vote_result.append({'question_id': int(que_id), 'answer': ans})
-
-    fname = os.path.join(args.model_dir, 'result-conf.json')
-    json.dump(conf_result, open(fname, 'w'))
     fname = os.path.join(args.model_dir, 'result-vote.json')
     json.dump(vote_result, open(fname, 'w'))
 
